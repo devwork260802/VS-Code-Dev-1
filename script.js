b68bc9273f75bf6fb80c43dee6e4126ebcfbb675
Dev Gupta <devwork260802@gmail.com>
2024-10-03 09:40:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-body {
-background-color: #111;
-overflow: hidden;
-text-align:center;
-display: flex;
-align-items: center;
-justify-content: center;
+let select = s => document.querySelector(s),
+selectAll = s => document.querySelectorAll(s),
+mainSVG = select('#mainSVG')
+gsap.set('svg', {
+visibility: 'visible'
+})
+gsap.set('#icon', {
+transformOrigin: '50% 50%'
+})
+let state = true;
+let doClick = () => {
+let tl = gsap.timeline({
+defaults: {
+ease: 'elastic(0.2, 0.48)',
+duration: 0.4
 }
-body,
-html {
-height: 100%;
-width: 100%;
-margin: 0;
-padding: 0;
+});
+if(state) {
+tl.to('line', {
+drawSVG: '0% 100%',
+x: 0,
+y: -0
+})
+.to('#icon', {
+scale: 1,
+y: 5,
+fill: '#44484D',
+}, 0)
+.fromTo('#lens', {
+x: 0
+}, {
+x: -3
+}, 0)
+} else {
+tl.to('line', {
+drawSVG: '50% 50%',
+x: 25,
+y: 19
+})
+.to('#icon', {
+y: 0,
+scale: 1.3,
+fill: '#555b60'
+}, 0)
+.fromTo('#lens', {
+x: -3
+}, {
+x: 0,
+ease: 'expo',
+}, 0)
 }
-svg {
-width: 100%;
-height: 100%;
-visibility: hidden;
-cursor: pointer;
 }
+mainSVG.onclick = () => {
+gsap.killTweensOf(mainSVG.onclick);
+state = !state;
+doClick();
+}
+gsap.delayedCall(2, mainSVG.onclick)
